f13ef7820823a2298987ab3d299330d3ad3ab7bd
Md. Abu Sakib Chowdhury <sakib21eee@gmail.com>
2024-09-25 00:16:06 +0600

    blog

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.getElementById('blog-info').addEventListener('click', function (event) {
+    event.preventDefault();
+    window.location.href = './blog.html'
+})
+document.getElementById('home-info').addEventListener('click', function (event) {
+    event.preventDefault();
+    window.location.href = './index.html'
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          green: '#B4F461',
+          navBg: '#F9F7F3',
+          gray: '#f3f4f6',
+          borderBg: '#1111111A'
+      }
+      },
     },
     plugins: [],
   }
